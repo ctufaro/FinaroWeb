59fcd5e42e61e587844c5e95c182957e1c40a71e
Chris Tufaro <ctufaro@gmail.com>
2019-02-12 13:46:02 -0500

    Layout Changes

--- js/exchange.js
+++ js/exchange.js
@@ -62,13 +62,13 @@ const vm = new Vue({
             
             initStaticDataTable('tblmyorders', myorders, 
                 (row,data,dataIndex)=>{
-                    $(row).addClass(data[4]==1?$(row).addClass('gains'):$(row).addClass('losses'));
-                }, [{"targets": 0, "width": "22%"},{"targets": 1, "width": "22%"},{"targets": 2, "width": "22%"},{"targets": 3, "width": "22%"}]);
+                    $(row).addClass(data[5]==1?$(row).addClass('gains'):$(row).addClass('losses'));
+                }, [{"targets": 0, "width": "30%"},{"targets": 1, "width": "10%"},{"targets": 2, "width": "20%"},{"targets": 3, "width": "20%"},{"targets": 4, "width": "20%"}]);
             
             initStaticDataTable('tblhistory',  tradehistory, 
                     (row,data,dataIndex)=>{
-                    $(row).addClass(data[3]==1?$(row).addClass('gains'):$(row).addClass('losses'));
-                }, [{"targets": 0, "width": "33%"},{"targets": 1, "width": "33%"},{"targets": 2, "width": "33%"}]);
+                    $(row).addClass(data[4]==1?$(row).addClass('gains'):$(row).addClass('losses'));
+                }, [{"targets": 0, "width": "35%"},{"targets": 1, "width": "12%"},{"targets": 2, "width": "12%"},{"targets": 3, "width": "22%"}]);
             
             LoadingComplete();
         });

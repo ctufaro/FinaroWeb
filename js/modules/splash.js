adc5ace44ad586369f8a33859d1069f8a948dcd2
christufaro <ctufaro@gmail.com>
2019-04-07 08:52:04 -0400

    Player stats

--- js/modules/splash.js
+++ js/modules/splash.js
@@ -2,6 +2,7 @@ export default class Splash{
 
     static init(vm) {
         $('.slide2').hide();
+        $('.slide3').hide();
         $('#splashModal').modal('show');  
         $('.slide1').show();      
         $('.slide1').addClass("animated bounceInLeft");
@@ -12,13 +13,23 @@ export default class Splash{
     static start(teamLeague,id){
         Splash.vm.selectTeamPlayer(teamLeague,id);
         $('.slide1').hide();
+        $('.slide3').hide();
         $('.slide2').show();
         $('.slide2').addClass("animated bounceInLeft");
         Splash.vm.splash.title = "Select team/player";
     }
 
-    static restart(){
+    static player(){
+        $('.slide1').hide();
         $('.slide2').hide();        
+        $('.slide3').show();      
+        $('.slide3').addClass("animated bounceInLeft");         
+        Splash.vm.splash.title = "Player Stat";
+    }    
+
+    static restart(){
+        $('.slide2').hide();  
+        $('.slide3').hide();      
         $('.slide1').show();      
         $('.slide1').addClass("animated bounceInLeft"); 
         Splash.vm.splash.title = "Select a league";

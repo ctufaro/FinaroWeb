63ed019e6e4b53655ccb53132f7375a8472f926d
christufaro <ctufaro@gmail.com>
2019-04-06 22:48:49 -0400

    Renamed

--- js/exchange.js
+++ js/exchange.js
@@ -6,7 +6,7 @@ import DTLeaguePlayer from './modules/datatables/dtleagueplayer.js';
 import DTHistory from './modules/datatables/dthistory.js';
 import DTMyOrders from './modules/datatables/dtmyorders.js';
 import DTBuySell from './modules/datatables/dtbuysell.js';
-
+import Splash from './modules/splash.js';
 
 const etherUrl = "https://ropsten.etherscan.io";
 const useLocalHost = false;
